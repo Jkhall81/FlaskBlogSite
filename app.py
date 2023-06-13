ea607408924bac2603747c62767fc083e39f01cb
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-13 11:28:00 -0700

    form data from add.html is now being grabbed and saved into the data.json file, also added a function to generate an id number for new posts.

--- app.py
+++ app.py
@@ -1,24 +1,65 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import json
 
-
 app = Flask(__name__)
-with open('data.json', 'r') as f:
-    blog_posts = json.load(f)
+
+
+def open_data(filename):
+    with open(filename, 'r') as f:
+        blog_posts = json.load(f)
+        return blog_posts
+
+
+def save_data(data):
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+
+def generate_new_id(blog_posts):
+    if blog_posts:
+        last_post = blog_posts[-1]['id']
+        new_id = last_post + 1
+    else:
+        new_id = 1
+    return new_id
 
 
 @app.route('/')
 def index():
+    blog_posts = open_data('data.json')
     return render_template('index.html', posts=blog_posts)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        # we will fill this in later
-        pass
+        # grab the posted data
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        blog_posts = open_data('data.json')
+
+        new_id = generate_new_id(blog_posts)
+
+        # create new post entry
+        new_post = {
+            'id': new_id,
+            'author': author,
+            'title': title,
+            'content': content
+        }
+
+        # append new post
+        blog_posts.append(new_post)
+
+        # save json
+        save_data(blog_posts)
+
+        return redirect('/')
+
     return render_template('add.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
